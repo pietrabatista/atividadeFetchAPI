5f031def10d78d109810352735b483a30fc96905
Pietra Batista <pietra.batista@sou.inteli.edu.br>
2024-05-21 11:19:42 -0300

    .

---
+++ script1.js
@@ -0,0 +1,17 @@
+// fazer botão escutar evento de click e chamar fetchData
+
+function fetchData() {
+    const url = 'https://jsonplaceholder.typicode.com/posts'; // URL da API pública
+
+    if (self.fetch) {
+        console.log("fetch disponível")
+    }
+
+    // fazer fetch na url, fazendo com informações sejam disponibilizadas e também tratando erros
+}
+
+function displayData(data) {
+    // acessa div de resultados
+
+    // itera sobre os dados em data e preenche a div do html
+}
---
+++ script2.js
@@ -0,0 +1,14 @@
+// fazer botão escutar evento de click e chamar fetchImage
+
+function fetchImage() {
+    const url = 'https://source.unsplash.com/random/600x400'; // URL da imagem pública
+    // const url = <arquivo local>
+
+    // fazer fetch na url, fazendo com que a imagem seja exibida e também tratando erros
+}
+
+function displayImage(imageUrl) {
+    // acessa div de resultados
+
+    // preenche a div com a imagem e adiciona
+}
